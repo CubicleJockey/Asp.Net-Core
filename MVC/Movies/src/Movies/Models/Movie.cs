5c1cef02365b7a6c3f5faac9b67f7b68cf3e0918
André Davis <CubicleJockey@users.noreply.github.com>
2016-12-29 10:56:35 -0800

    Adding Validation

--- MVC/Movies/src/Movies/Models/Movie.cs
+++ MVC/Movies/src/Movies/Models/Movie.cs
@@ -6,12 +6,27 @@ namespace Movies.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        
+        [DataType(DataType.Date)]
         [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Rating { get; set; }
     }
 }
