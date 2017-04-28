2ca366a4fa01d55f3261b276c5292fe5d7fa03be
André Davis <CubicleJockey@users.noreply.github.com>
2017-04-27 22:08:46 -0700

    App launches with errors. Next checkin will be working through the errors till we get the basic page.

--- AngularWithWebPack/Template/Template/Startup.cs
+++ AngularWithWebPack/Template/Template/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -35,6 +36,10 @@ namespace Template
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
             }
 
             app.UseDefaultFiles();
