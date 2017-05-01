b1920c5f5a5ad456fa90b8af63509d5ff31a07d2
André Davis <CubicleJockey@users.noreply.github.com>
2017-05-01 13:59:11 -0700

    Trying to set up tests

--- AngularWithWebPack/Template/Template/ClientApp/config/karma.config.js
+++ AngularWithWebPack/Template/Template/ClientApp/config/karma.config.js
@@ -1,1 +1,37 @@
-﻿
+﻿var webpackConfig = require('./webpack.qa');
+
+module.exports = function(config) {
+    const Config = {
+        basePath: '',
+
+        frameworks: ['jasmine'],
+
+        files: [
+            { pattern: './ClientApp/config/karma-test-shim.js', watched: false }
+        ],
+
+        preprocessors: {
+            './ClientApp/config/karma-test-shim.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackConfig,
+
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+
+        webpackServer: {
+            noInfo: true
+        },
+
+        reporters: ['kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_VERBOSE,
+        autoWatch: true,
+        browsers: ['Opera'],
+        singleRun: true
+    };
+
+    config.set(Config);
+};
